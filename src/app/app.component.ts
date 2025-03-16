d6ceb78f0bb144767488d7710c76686bb78f511d
ariful-2019331062 <ariful62@student.sust.edu>
2025-03-17 03:53:44 +0600

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './compontents/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './home/home.component';
 
 @Component({
@@ -8,9 +8,17 @@ import { HomeComponent } from './home/home.component';
   imports: [RouterOutlet, HeaderComponent, HomeComponent],
   template: `
     <app-header> </app-header>
-    <app-home> </app-home>
+    <main>
+      <app-home />
+    </main>
   `,
-  styles: [],
+  styles: [
+    `
+      main {
+        padding: 16px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'first-ng-app';
