1e6d070eabd85deb4255c9f3e708b40586bf1ea3
ariful-2019331062 <arifulfarhad300@gmail.com>
2025-03-18 17:22:05 +0600

    todo app completed

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,10 +2,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../components/model/todo.type';
 import { catchError } from 'rxjs';
+import { NgIf } from '@angular/common';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [NgIf, TodoItemComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
@@ -27,5 +29,15 @@ export class TodosComponent implements OnInit {
       .subscribe((todos) => {
         this.todoItems.set(todos);
       });
+  }
+  updateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if (todo.id == todoItem.id) {
+          return { ...todo, completed: !todoItem.completed };
+        }
+        return todo;
+      });
+    });
   }
 }
